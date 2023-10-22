2c3c6077aaf6a0c545c6d90e04d7a7a1eb9597c5
dlasap <lasapdominic@gmail.com>
2023-10-22 22:52:01 +0800

    add build fix

--- app/components/ProfileContent/Contents/HomeContent.jsx
+++ app/components/ProfileContent/Contents/HomeContent.jsx
@@ -6,7 +6,7 @@ const HomeContent = () => {
       <h2>Hey There!</h2>
 
       <p>
-        I'm a chemical engineering alumnus from the Cebu Institute of Technology University, I currently serve as a process engineer at Onsemi, a
+        I&apos;m a chemical engineering alumnus from the Cebu Institute of Technology University, I currently serve as a process engineer at Onsemi, a
         prominent player in the semiconductor industry.
       </p>
 
@@ -16,9 +16,9 @@ const HomeContent = () => {
       </p>
 
       <p>
-        In my professional vision, I aspire to be a key figure in Onsemi's expansion, consistently driving innovation to ensure the company's
-        continued leadership in semiconductor manufacturing. I am committed to motivating my colleagues to pursue excellence in the ever-evolving
-        domain of semiconductor engineering.
+        In my professional vision, I aspire to be a key figure in Onsemi&apos;s expansion, consistently driving innovation to ensure the
+        company&apos;s continued leadership in semiconductor manufacturing. I am committed to motivating my colleagues to pursue excellence in the
+        ever-evolving domain of semiconductor engineering.
       </p>
     </div>
   );
